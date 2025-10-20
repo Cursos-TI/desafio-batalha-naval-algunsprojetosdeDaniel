e374c9d9ef55beb5a91d80a622b90b72fb23b9c9
algunsprojetosdeDaniel <lojamarcketplaces@gmail.com>
2025-10-20 14:22:45 +0000

    diagonal

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,6 +4,22 @@ int main () {
 
     printf("   A B C D E F G H I J\n");
 
+    mar[4][7] = 3;
+    mar[4][8] = 3;
+    mar[4][9] = 3;
+
+    mar[0][2] = 3;
+    mar[1][2] = 3;
+    mar[2][2] = 3;
+
+    mar[6][1] = 3;
+    mar[7][2] = 3;
+    mar[8][3] = 3;
+
+    mar[1][6] = 3;
+    mar[2][7] = 3;
+    mar[3][8] = 3;
+
     for (int i = 0; i < 9; i++) {
         printf(" %d ", i + 1);
         for (int j = 0; j < 10; j++) {
